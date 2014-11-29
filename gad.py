d4495badc655b0ba645dc1bdd5898b29d097d632
D Strong <dstrong@glyx.co.uk>
2014-11-29 20:21:08 +0000

    License added

--- gad.py
+++ gad.py
@@ -1,9 +1,21 @@
 #!/usr/bin/env python
-## This script is a python rewrite of the gandi automatic dns script.
-## https://github.com/brianpcurran/gandi-automatic-dns
-## 
-## Note that this script relies on icanhazip.com for external ip
-## detection. If the service goes down, this script won't work.
+# This script is intended for use as a cron job to update the A records
+# of Gandi zone files.
+# Copyright (C) 2014 Daniel Strong
+# dstrong@glyx.co.uk
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software Foundation,
+# Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301  USA
 
 import sys
 import urllib.request
